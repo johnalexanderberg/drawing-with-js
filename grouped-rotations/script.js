e188ee8c2db8af28cb1bf5dd935a6e092ae7b6d1
Alexander Berg <72527002+johnalexanderberg@users.noreply.github.com>
2021-09-07 15:24:43 +0200

    more patterns

--- grouped-rotations/script.js
+++ grouped-rotations/script.js
@@ -9,31 +9,37 @@ const two = new Two(params);
 
 two.appendTo(container);
 
-const numberOfShapes = 12;
+let numberOfShapes = 12;
 const plotRadius = 150;
 const shapes = [];
 shapes.length = numberOfShapes;
+let group;
 
-for(let i = 0; i < numberOfShapes; i++){
-    const angle = (Math.PI * 2) * i / numberOfShapes;
-    const x = plotRadius * Math.cos(angle);
-    const y = plotRadius * Math.sin(angle);
-    const width = 50;
-    const height = 50;
-const shape = two.makeRectangle(x, y, width, height);
-    shape.fill = '#31f9b3';
-    shape.noStroke();
-    shapes[i] = shape;
-    shape.rotation = angle + 2*i;
+function spawnShapes() {
+
+    for (let i = 0; i < numberOfShapes; i++) {
+        const angle = (Math.PI * 2) * i / numberOfShapes;
+        const x = (plotRadius) * Math.cos(angle);
+        const y = (plotRadius) * Math.sin(angle);
+        const width = params.width / numberOfShapes;
+        const height = params.width / numberOfShapes;
+        const shape = two.makeRectangle(x, y, width, height);
+        shape.fill = '#31f9b3';
+        shape.noStroke();
+        shapes[i] = shape;
+        shape.rotation = angle + 2 * i;
+    }
+
+    group = two.makeGroup(shapes);
+    group.translation.set(250, 250);
 }
 
-const group = two.makeGroup(shapes);
-group.translation.set(250, 250);
+spawnShapes();
 
 
 const onUpdate = () => {
 
-    for(let i = 0; i < shapes.length; i++){
+    for (let i = 0; i < shapes.length; i++) {
         shapes[i].rotation += 0.01;
 
     }
@@ -44,3 +50,11 @@ two.bind('update', onUpdate)
 
 two.play();
 
+const handleClick = () => {
+    numberOfShapes = numberOfShapes * 2;
+    spawnShapes();
+    console.log('clicked');
+};
+
+
+container.addEventListener('click', handleClick);
